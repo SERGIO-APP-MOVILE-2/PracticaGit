628212465d810e8aa993fe55d3d01e804b933782
SanaranjoSRV <developer.srv.b01@gmail.com>
2025-11-19 10:40:45 -0500

    agregar ruta

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 // app.ts
 import express from 'express';
+import { request, response } from 'express';
 const app = express();
 app.use(express.json());
+// Ruta de prueba
+app.get('/ping', (req, res) => {
+	res.json({ message: 'pong' });
+});
 export default app;
